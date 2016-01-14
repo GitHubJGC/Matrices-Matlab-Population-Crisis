ff10096daf5e99363994430552d9269dae94f0ea
GitHubJGC <nujujuju@yahoo.co.uk>
2016-01-14 22:40:21 +0000

    Create interventionfreq.m

---
+++ interventionfreq.m
@@ -0,0 +1,17 @@
+
+flagi = 1;
+while flagi ~= 0;
+    
+i = input('Choose how often you would like to change the health/educ ratio');
+
+if i ~=0:n
+    display 'must be a number from 0 to n'
+    flagi = 1;    
+    
+else
+    
+    flagi = 0;
+    
+end
+
+end
