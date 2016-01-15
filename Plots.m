54a15f4236d8b0c7f825b6ea184db51f19ab3bcd
GitHubJGC <nujujuju@yahoo.co.uk>
2016-01-15 18:17:23 +0000

    Update Plots.m

--- Plots.m
+++ Plots.m
@@ -1,19 +1,20 @@
 figure;
 subplot(2,3,1);
 plot(0:1:n,PT);
-title('Graph 1: Total population in each period');
+
+title('Total population');
 xlabel('Period/10 years');
 ylabel('Total population');
 
 subplot(2,3,2);
 plot(0:1:n,SPPM,'r-*');
-title('Graph 2: Spending per person after pension costs in each period');
+title('Spending per person after pension');
 xlabel('Period/10 years');
 ylabel('Spending per person');
 
 subplot(2,3,3);
-bar(q);
-title('Graph 3: Propotion of total population per age group');
+bar(q), colormap(summer);
+title('Age group as proportion of total population');
 xlabel('Age group');
 ylabel('Proportion of total population')
 legend('Population in period 1','Population in final period');
@@ -26,22 +27,34 @@ plot(0:1:n,X(g,:),'color',rand(1,3));
 hold on;
 end
 
-title('Graph 3: Propotion of total population per age group');
+title('Population per age group');
 xlabel('Periods/10 years');
 ylabel('Number of people');
 legend('0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+');
 
-HealthCost = (HealthP/100)*SPPM;
-EducCost = (EducP/100)*SPPM;
-SplitCosts = [HealthCost;EducCost];
+%HealthCost = (HealthP/100)*SPPM;
+%EducCost = (EducP/100)*SPPM;
+%SplitCosts = [HealthCost;EducCost];
+%SplitCosts = HealthEducP/100*
 
 subplot(2,3,5);
 
 for z = 1:2
-plot(0:1:n,SplitCosts(z,:),'color',rand(1,3));
+plot(1:1:n,SplitCosts(z,:),'color',rand(1,3));
 hold on
 end
-title('Graph to show spending on Helathcare and Education');
+
+title('Healthcare/Education Spending');
 xlabel('Period/10 years');
 ylabel('Total budget to spend');
 legend('Healthcare','Education');
+
+subplot(2,3,6);
+
+%plot(1:1:n,HealthEducP);
+bar(HealthEducP, 'stacked');
+
+title('Percent of budget spent on Education and Healthcare');
+xlabel('Period/10 years');
+ylabel('% of budget');
+legend('Healthcare','Education');   
